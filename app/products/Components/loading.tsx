518bb27b3ab020eb058c44c78bf00b9363ef4a9e
Fabio Piras <fabio.piras@spindox.it>
2024-05-03 08:11:48 +0200

    chore(loading): loading implementation

--- app/products/Components/loading.tsx
+++ app/products/Components/loading.tsx
@@ -2,5 +2,5 @@ import React from "react"
 import styles from './styles/loading.module.css'
 
 export default function Loading() {
-    return <div className={styles.ldsRing}><div></div><div></div><div></div><div></div></div>
+    return <div className={styles.container}><div className={styles.ldsRing}><div></div><div></div><div></div><div></div></div></div>
   }
